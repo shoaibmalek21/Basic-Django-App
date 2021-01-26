d70f9fdd581d2fbcefb31e159274f305470ae0ef
Shoaib <shoaibmalek21@gmail.com>
2021-01-26 18:18:37 +0530

    first commit

---
+++ myproject_udemy/main/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class Main(models.Model):
+	name = models.TextField()
+
+	def __str__(self):
+		return self.name
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shoaib/python/django/myproject_udemy/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
